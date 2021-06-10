cf248c120f0ba73b4946b40592f520ed099f3116
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2021-06-10 17:41:46 +0530

    :sparkles: Add cards for fake data

---
+++ components/CardItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-card>
+      <b-card-text>
+        {{ data.title }}
+      </b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- components/StatusColumn.vue
+++ components/StatusColumn.vue
@@ -1,6 +1,19 @@
 <template>
   <b-col lg="4">
-    <h5>{{ data && data.status }}</h5>
+    <span class="d-flex align-items-center">
+      <h5 class="status-heading">{{ data.status }}</h5>
+      <b-badge pill variant="secondary" class="mx-2">
+        {{ data.tasks.length }}
+      </b-badge>
+      <h3 class="ml-auto text-secondary add-new-button">+</h3>
+    </span>
+    <CardItem
+      v-for="card in data.tasks"
+      :key="card.id"
+      :data="card"
+      class="my-3"
+    />
+    <nuxt-link to="/" class="link text-secondary"> + New </nuxt-link>
   </b-col>
 </template>
 
@@ -15,4 +28,16 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.status-heading {
+  font-weight: 600;
+}
+
+.link:hover {
+  text-decoration: none;
+}
+
+.add-new-button {
+  cursor: pointer;
+}
+</style>
